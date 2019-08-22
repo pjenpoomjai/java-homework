db4240e49e4df14b36b90340e5f865be3b40b26f
pjenpoomjai <pjenpoomjai@gmail.com>
2019-08-22 23:29:31 +0700

    part4 inverted index search

--- src/main/java/com/wongnai/interview/movie/search/InvertedIndexMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/InvertedIndexMovieSearchService.java
@@ -1,6 +1,6 @@
 package com.wongnai.interview.movie.search;
 
-import java.util.List;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
@@ -34,7 +34,59 @@ public class InvertedIndexMovieSearchService implements MovieSearchService {
 		// from inverted index for Star and for War so that you get movie ids 1,5,8 for Star and 2,5 for War. The result that
 		// you have to return can be union or intersection of those 2 sets of ids.
 		// By the way, in this assignment, you must use intersection so that it left for just movie id 5.
+		Iterable<Movie> movieIterable = movieRepository.findAll();
+		Map<Long, Movie> mapperMovie = new HashMap<>();
+		Map<String, ArrayList<Long>> mapper = new HashMap<>();
+		ArrayList arrayList;
+		for (Movie movie: movieIterable
+			 ) {
+			mapperMovie.put(movie.getId(), new Movie(movie.getName(), movie.getActors()));
+			for (String word : movie.getName().toLowerCase().split(" ")
+				 ) {
+				if (mapper.containsKey(word)){
+					if (!mapper.get(word).contains(movie.getId())){
+						mapper.get(word).add(movie.getId());
+					}
 
-		return null;
+				}
+				else{
+					arrayList = new ArrayList<>();
+					arrayList.add(movie.getId());
+					mapper.put(word, arrayList);
+				}
+
+			}
+
+		}
+		//counting
+		List<Movie> result = new ArrayList<>();
+		String[] words = queryText.toLowerCase().split(" ");
+		int max = words.length;
+		Map<Long,Integer> idsMap = new HashMap<>();
+		for (String word : words
+			 ) {
+			if (mapper.get(word)!=null){
+				for (Long ids : mapper.get(word)
+				) {
+					if (idsMap.containsKey(ids)){
+						idsMap.replace(ids,idsMap.get(ids)+1);
+					}
+					else {
+						idsMap.put(ids, 1);
+					}
+				}
+			}
+		//summary
+		}
+		Movie movie;
+		for (Long id : idsMap.keySet()
+			 ) {
+			if (idsMap.get(id).equals(max)){
+				movie = mapperMovie.get(id);
+				result.add(new Movie(movie.getName(), movie.getActors()));
+			}
+
+		}
+		return result;
 	}
 }
