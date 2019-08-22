8e9a33f2bbe034463f416183cd1a9273b6d1f004
pjenpoomjai <pjenpoomjai@gmail.com>
2019-08-22 08:23:46 +0700

    part 2 search Simple movie

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,7 +1,11 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
+import com.wongnai.interview.movie.external.MovieData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +23,23 @@ public class SimpleMovieSearchService implements MovieSearchService {
 		//TODO: Step 2 => Implement this method by using data from MovieDataService
 		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
 		// Please do not change @Component annotation on this class
-		return null;
+		Iterator<MovieData> movieDataIterator = movieDataService.fetchAll().iterator();
+		List<Movie> movieList = new ArrayList<>();
+		MovieData movieData;
+		String title;
+		while (movieDataIterator.hasNext()){
+			movieData = movieDataIterator.next();
+			title = movieData.getTitle().toLowerCase();
+			queryText = queryText.toLowerCase();
+			List<String> titleWords = Arrays.asList(title.split(" "));
+			for (String word : titleWords
+				 ) {
+				if (!title.equals(queryText) && word.equals(queryText)){
+					movieList.add(new Movie(movieData.getTitle(), movieData.getCast()));
+				}
+			}
+
+		}
+		return movieList;
 	}
 }
